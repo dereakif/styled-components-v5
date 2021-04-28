95f59508ab1274824b676d964215b6bd7b846c3b
akif dere <dereakif@gmail.com>
2021-04-28 19:56:18 +0300

    add: input and password toggler

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -2,7 +2,19 @@ import { PageLayout } from "components/common";
 import React from "react";
 
 function Home() {
-  return <PageLayout>Home</PageLayout>;
+  return (
+    <PageLayout>
+      <h1>Home</h1>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus
+        sodales nulla massa, at pretium velit facilisis eget. Nullam rhoncus
+        orci in finibus porta. Mauris facilisis vehicula metus, vitae lobortis
+        justo rhoncus a. Praesent molestie porttitor cursus. Pellentesque non
+        nisi nisi. Mauris in luctus felis. Etiam blandit pellentesque nisl, a
+        mollis est ornare sit amet.
+      </p>
+    </PageLayout>
+  );
 }
 
 export default Home;
--- src/components/pages/Login.js
+++ src/components/pages/Login.js
@@ -1,8 +1,50 @@
-import { PageLayout } from "components/common";
-import React from "react";
+import { PageLayout, Input } from "components/common";
+import React, { useState } from "react";
+import styled from "styled-components";
+
+const Form = styled.form`
+  width: 100%;
+  max-width: 400px;
+  background: white;
+  border: 1px solid #eee;
+  padding: 16px;
+  box-sizing: border-box;
+  color: black;
+  border-radius: 4px;
+`;
 
 function Login() {
-  return <PageLayout>Login</PageLayout>;
+  const [formFields, setFormFields] = useState({ username: "", password: "" });
+  const [showPassword, setShowPassword] = useState(false);
+  function handleInputChange(e) {
+    e.persist();
+    setFormFields((s) => ({ ...s, [e.target.name]: e.target.value }));
+  }
+  function passwordToggler() {
+    setShowPassword((s) => !s);
+  }
+  return (
+    <PageLayout>
+      <h1>Login</h1>
+      <Form>
+        <Input
+          value={formFields.username}
+          onChange={handleInputChange}
+          name="username"
+          type="text"
+          placeholder="username"
+        />
+        <Input
+          value={formFields.password}
+          onChange={handleInputChange}
+          name="password"
+          type={showPassword ? "text" : "password"}
+          placeholder="password"
+        />
+      </Form>
+      <button onClick={passwordToggler}>passtoggler</button>
+    </PageLayout>
+  );
 }
 
 export default Login;
